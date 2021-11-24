f0c7fa9bac859ce4ca98cf7837a6fe7f5d62f51b
SHA-2084 <2048.sha@gmail.com>
2021-11-24 23:20:23 +0100

    [test] Add hardhat tests

---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import "@nomiclabs/hardhat-vyper";
+import 'hardhat-deploy';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import 'dotenv/config';
+import {HardhatUserConfig} from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.5.0",
+  vyper: {
+    version: "0.2.12",
+  },
+  networks: {
+    hardhat: {
+      initialDate: "2021-11-20T00:00:00",
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+        blockNumber: parseInt(<string>process.env.ETH_MAINNET_BLOCK_NUMBER),
+      }
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_ROPSTEN_API_KEY}`,
+      chainId: 3,
+      gasPrice: 20000000000,
+      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
+    eth_mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+      chainId: 1,
+      gasPrice: 40000000000,
+      accounts: [`0x${process.env.ETH_MAINNET_PRIVATE_KEY}`],
+    },
+    fantom_mainnet: {
+      url: `https://rpc.ftm.tools/`,
+      chainId: 250,
+      gasPrice: 200000000000,
+      accounts: [`0x${process.env.FANTOM_MAINNET_PRIVATE_KEY}`],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    currency: 'EUR',
+    coinmarketcap: process.env.CMC_API_KEY || undefined,
+    enabled: !!process.env.REPORT_GAS,
+    showTimeSpent: true,
+  },
+  mocha: {
+    timeout: 200000
+  }
+};
+
+export default config;
---
+++ test/EndToEndScenario.ts
@@ -0,0 +1,129 @@
+import { expect } from "chai";
+import { ethers } from 'hardhat';
+const hre = require("hardhat");
+
+import {
+    VotingEscrow__factory,
+    GaugeController__factory,
+    VotingEscrow,
+    GaugeController,
+    Minter__factory,
+    Minter,
+    RewardPolicyMaker__factory,
+    RewardPolicyMaker,
+    Treasury__factory,
+    Treasury,
+    LiquidityGaugeV4__factory,
+    LiquidityGaugeV4,
+    ERC20TOKEN__factory,
+    ERC20TOKEN
+} from "../typechain";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import exp from "constants";
+
+describe("FeeConverter contract", function () {
+
+    const DAY = 86400;
+    const A_YEAR_FROM_NOW = 1668902400
+
+    let erc20Factory: ERC20TOKEN__factory;
+
+    let hnd: ERC20TOKEN;
+    let hndLpToken: ERC20TOKEN;
+
+    let treasuryFactory: Treasury__factory;
+    let treasury: Treasury;
+
+    let minterFactory: Minter__factory;
+    let minter: Minter;
+
+    let rewardPolicyMakerFactory: RewardPolicyMaker__factory;
+    let rewardPolicyMaker: RewardPolicyMaker;
+
+    let votingEscrowFactory: VotingEscrow__factory;
+    let votingEscrow: VotingEscrow;
+
+    let gaugeControllerFactory: GaugeController__factory;
+    let gaugeController: GaugeController;
+
+    let gaugeFactory: LiquidityGaugeV4__factory;
+    let gauge1: LiquidityGaugeV4;
+    let gauge2: LiquidityGaugeV4;
+
+    let owner: SignerWithAddress;
+    let alice: SignerWithAddress;
+    let bob: SignerWithAddress;
+
+    beforeEach(async function () {
+
+        [owner, alice, bob] =
+            await ethers.getSigners();
+
+        erc20Factory = <ERC20TOKEN__factory>await ethers.getContractFactory("ERC20TOKEN");
+        rewardPolicyMakerFactory = <RewardPolicyMaker__factory>await ethers.getContractFactory("RewardPolicyMaker");
+        treasuryFactory = <Treasury__factory>await ethers.getContractFactory("Treasury");
+        votingEscrowFactory = <VotingEscrow__factory>await ethers.getContractFactory("VotingEscrow");
+        gaugeControllerFactory = <GaugeController__factory>await ethers.getContractFactory("GaugeController");
+        minterFactory = <Minter__factory>await ethers.getContractFactory("Minter");
+        gaugeFactory = <LiquidityGaugeV4__factory>await ethers.getContractFactory("LiquidityGaugeV4");
+
+        hnd = await erc20Factory.deploy("Hundred Finance", "HND", 18, 0);
+        hndLpToken = await erc20Factory.deploy("Hundred Finance Lp token", "hETH", 18, 0);
+
+        rewardPolicyMaker = await rewardPolicyMakerFactory.deploy(DAY * 7);
+
+        treasury = await treasuryFactory.deploy(hnd.address);
+        votingEscrow = await votingEscrowFactory.deploy(hnd.address, "Voting locked HND", "veHND", "1.0");
+        gaugeController = await gaugeControllerFactory.deploy(hnd.address, votingEscrow.address);
+        minter = await minterFactory.deploy(treasury.address, gaugeController.address);
+        gauge1 = await gaugeFactory.deploy(hndLpToken.address, minter.address, owner.address, rewardPolicyMaker.address);
+        gauge2 = await gaugeFactory.deploy(hndLpToken.address, minter.address, owner.address, rewardPolicyMaker.address);
+
+        await treasury.set_minter(minter.address);
+
+        await gaugeController["add_type(string,uint256)"]("Liquidity", ethers.utils.parseEther("10"));
+        await gaugeController["add_gauge(address,int128,uint256)"](gauge1.address, 0, 1);
+        await gaugeController["add_gauge(address,int128,uint256)"](gauge2.address, 0, 1);
+
+        await hnd.mint(alice.address, ethers.utils.parseEther("10000"));
+        await hnd.mint(bob.address, ethers.utils.parseEther("1000"));
+
+        await hnd.mint(treasury.address, ethers.utils.parseEther("10000"));
+
+        await hndLpToken.mint(alice.address, ethers.utils.parseEther("10"));
+        await hndLpToken.mint(bob.address, ethers.utils.parseEther("10"));
+
+        await rewardPolicyMaker.set_rewards_at(3, ethers.utils.parseEther("100"));
+
+    });
+
+    describe("Locked voting amount", function () {
+        it("Should reflect on the amount of claimable HND per user", async function () {
+
+            await hnd.connect(alice).approve(votingEscrow.address, ethers.utils.parseEther("10000000"));
+            await hnd.connect(bob).approve(votingEscrow.address, ethers.utils.parseEther("10000000"));
+
+            await votingEscrow.connect(alice).create_lock(ethers.utils.parseEther("10000"), A_YEAR_FROM_NOW);
+            await votingEscrow.connect(bob).create_lock(ethers.utils.parseEther("1000"), A_YEAR_FROM_NOW);
+
+            await gaugeController.connect(alice).vote_for_gauge_weights(gauge1.address, 1000);
+            await gaugeController.connect(bob).vote_for_gauge_weights(gauge2.address, 1000);
+
+            await hndLpToken.connect(alice).approve(gauge1.address, ethers.utils.parseEther("10000000"));
+            await hndLpToken.connect(bob).approve(gauge2.address, ethers.utils.parseEther("10000000"));
+
+            await gauge1.connect(alice)["deposit(uint256)"](ethers.utils.parseEther("10"));
+            await gauge2.connect(bob)["deposit(uint256)"](ethers.utils.parseEther("10"));
+
+            await ethers.provider.send("evm_increaseTime", [DAY * 30]);
+            await ethers.provider.send("evm_mine", []);
+
+            await minter.connect(alice).mint(gauge1.address);
+            await minter.connect(bob).mint(gauge2.address);
+
+            expect(await hnd.balanceOf(alice.address)).equals(ethers.utils.parseEther("90.909090909091762508"));
+            expect(await hnd.balanceOf(bob.address)).equals(ethers.utils.parseEther("9.090909090908029388"));
+        });
+    });
+
+});
