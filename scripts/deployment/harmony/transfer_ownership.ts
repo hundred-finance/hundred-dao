37c90ee2b3ec92fa4463ef4a6660f6d54786386a
SHA-2084 <2048.sha@gmail.com>
2021-12-26 11:35:12 +0100

    [deploy] set gnosis safe address on harmony transfer ownership script

--- scripts/deployment/harmony/transfer_ownership.ts
+++ scripts/deployment/harmony/transfer_ownership.ts
@@ -1,6 +1,6 @@
 import {transferOwnership} from "../common/deploy";
 
-transferOwnership("TO-FILL", "harmony")
+transferOwnership("0xC79354D37BD6B290Acc52C58798ACaa3CecD0b1B", "harmony")
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
