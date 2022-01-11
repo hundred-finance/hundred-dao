726404dc9b0f199ae5c94301c592b748be71db1a
SHA-2084 <2048.sha@gmail.com>
2022-01-11 22:58:29 +0100

    [deploy] add airdrop simulation script

---
+++ scripts/users/calculateAirdropAmounts.ts
@@ -0,0 +1,37 @@
+import fs from "fs";
+import Balances from "./balances.json";
+import {BigNumber} from "ethers";
+
+const AirdropAmount = 18750 * 1e18
+
+calculateAirdropAmounts()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
+
+async function calculateAirdropAmounts() {
+    let airdropAmounts: Map<string, string> = new Map<string, string>()
+
+    for (let i = 0; i < Balances.length; i++) {
+        let amount = Math.round(Balances[i].ve_hnd_share * AirdropAmount)
+        if (amount >= 100000000000000000) {
+            airdropAmounts.set(
+                Balances[i].user,
+                BigInt(Math.round(Balances[i].ve_hnd_share * AirdropAmount)).toString()
+            )
+        }
+    }
+
+    fs.writeFileSync(`./scripts/users/airdrop.json`,
+        JSON.stringify(mapToObj(airdropAmounts), null, 4)
+    );
+}
+
+function mapToObj(m: Map<string, string>) {
+    return Array.from(m).reduce((obj: any, [key, value]) => {
+        obj[key] = value;
+        return obj;
+    }, {});
+}
