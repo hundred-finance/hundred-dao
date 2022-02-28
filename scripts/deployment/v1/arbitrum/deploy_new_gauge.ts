e4c8c485f0927b75353282c574e3fdb15e858808
SHA-2084 <2048.sha@gmail.com>
2022-02-28 17:52:09 +0100

    add hDAI arbitrum gauge

--- scripts/deployment/v1/arbitrum/deploy_new_gauge.ts
+++ scripts/deployment/v1/arbitrum/deploy_new_gauge.ts
@@ -1,10 +1,10 @@
 import {deployNewGauge} from "../common/deploy";
 
 deployNewGauge(
-    "TO-SET-NEW-ADMIN",
+    "0xb982841d8Caf7cCc1b5c8ec414347316F54A06c9",
     "arbitrum",
-    "lp-token-address",
-    "lp-token-symbol"
+    "0x6bb6ebCf3aC808E26545d59EA60F27A202cE8586",
+    "hDAI"
 )
     .then(() => process.exit(0))
     .catch(error => {
