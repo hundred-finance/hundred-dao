7e131d3dfd98b886edbbad4231e3a5b7886b1b93
SHA-2084 <2048.sha@gmail.com>
2022-03-20 18:50:13 +0100

    deploy MirrorGate to test nets

--- scripts/deployment/v2/avalanche_fuji/full_deploy.ts
+++ scripts/deployment/v2/avalanche_fuji/full_deploy.ts
@@ -5,12 +5,13 @@ const POOLS: any = []
 
 const HUNDRED_TOKEN = "0x10010078a54396F62c96dF8532dc2B4847d47ED3";
 const ADMIN = "0x8286dC6dF929C4BfA4f6951caB4dAe2EC02d4D72"
+const LAYER_ZERO_ENDPOINT = "0x93f54D755A063cE7bB9e6Ac47Eccc8e33411d706"
 
 const network = hre.hardhatArguments.network;
 if (!network) {
     console.error("please provide valid network");
 } else {
-    deploy(HUNDRED_TOKEN, POOLS, network, ADMIN)
+    deploy(HUNDRED_TOKEN, POOLS, network, ADMIN, "deployments", LAYER_ZERO_ENDPOINT)
         .then(() => process.exit(0))
         .catch(error => {
             console.error(error);
--- scripts/deployment/v2/optimism_kovan/full_deploy.ts
+++ scripts/deployment/v2/optimism_kovan/full_deploy.ts
@@ -5,12 +5,13 @@ const POOLS: any = []
 
 const HUNDRED_TOKEN = "0x10010078a54396F62c96dF8532dc2B4847d47ED3";
 const ADMIN = "0x8286dC6dF929C4BfA4f6951caB4dAe2EC02d4D72"
+const LAYER_ZERO_ENDPOINT = "0x72aB53a133b27Fa428ca7Dc263080807AfEc91b5"
 
 const network = hre.hardhatArguments.network;
 if (!network) {
     console.error("please provide valid network");
 } else {
-    deploy(HUNDRED_TOKEN, POOLS, network, ADMIN)
+    deploy(HUNDRED_TOKEN, POOLS, network, ADMIN, "deployments", LAYER_ZERO_ENDPOINT)
         .then(() => process.exit(0))
         .catch(error => {
             console.error(error);
