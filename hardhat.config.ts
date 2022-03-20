053c4d271bf378f1551647537f35b75aea3b7269
SHA-2084 <2048.sha@gmail.com>
2022-03-20 21:20:01 +0100

    update dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -62,7 +62,47 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      ropsten: process.env.ETHERSCAN_API_KEY,
+      rinkeby: process.env.ETHERSCAN_API_KEY,
+      goerli: process.env.ETHERSCAN_API_KEY,
+      kovan: process.env.ETHERSCAN_API_KEY,
+      // binance smart chain
+      bsc: "YOUR_BSCSCAN_API_KEY",
+      bscTestnet: "YOUR_BSCSCAN_API_KEY",
+      // huobi eco chain
+      heco: "YOUR_HECOINFO_API_KEY",
+      hecoTestnet: "YOUR_HECOINFO_API_KEY",
+      // fantom mainnet
+      opera: "YOUR_FTMSCAN_API_KEY",
+      ftmTestnet: "YOUR_FTMSCAN_API_KEY",
+      // optimism
+      optimisticEthereum: process.env.OPTIMISM_SCAN_API_KEY,
+      optimisticKovan: process.env.OPTIMISM_SCAN_API_KEY,
+      // polygon
+      polygon: "YOUR_POLYGONSCAN_API_KEY",
+      polygonMumbai: "YOUR_POLYGONSCAN_API_KEY",
+      // arbitrum
+      arbitrumOne: "YOUR_ARBISCAN_API_KEY",
+      arbitrumTestnet: "YOUR_ARBISCAN_API_KEY",
+      // avalanche
+      avalanche: process.env.AVALANCHE_SCAN_API_KEY,
+      avalancheFujiTestnet: process.env.AVALANCHE_SCAN_API_KEY,
+      // moonbeam
+      moonbeam: "YOUR_MOONBEAM_MOONSCAN_API_KEY",
+      moonriver: "YOUR_MOONRIVER_MOONSCAN_API_KEY",
+      moonbaseAlpha: "YOUR_MOONBEAM_MOONSCAN_API_KEY",
+      // harmony
+      harmony: "YOUR_HARMONY_API_KEY",
+      harmonyTest: "YOUR_HARMONY_API_KEY",
+      // xdai and sokol don't need an API key, but you still need
+      // to specify one; any string placeholder will work
+      xdai: "api-key",
+      sokol: "api-key",
+      aurora: "api-key",
+      auroraTestnet: "api-key",
+    }
   },
   gasReporter: {
     currency: 'EUR',
