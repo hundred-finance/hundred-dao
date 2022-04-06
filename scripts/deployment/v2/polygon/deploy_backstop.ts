11405a1215aa42986169625a44c40f9c66f3953d
SHA-2084 <2048.sha@gmail.com>
2022-04-06 16:51:18 +0200

    deploy backstop FRAX gauge on polygon

--- scripts/deployment/v2/polygon/deploy_backstop.ts
+++ scripts/deployment/v2/polygon/deploy_backstop.ts
@@ -5,6 +5,7 @@ const POOLS: any = [
     { id: "bhDAI", token: "0x998Bf304Ce9Cb215F484aA39d1177b8210078f49", weight: 1 },
     { id: "bhUSDC", token: "0x0F0dD66D2d6c1f3b140037018958164c6AB80d56", weight: 1 },
     { id: "bhUSDT", token: "0x1EcF1b0DE9b4c2D01554062eA2faB84b1917B41d", weight: 1 },
+    { id: "bhFRAX", token: "0x2DA13538056aFf0bFC81d3A4c6364B0a7e0f9feb", weight: 1 },
 ]
 
 const HUNDRED_TOKEN = "0x10010078a54396F62c96dF8532dc2B4847d47ED3";
