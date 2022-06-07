8d873c14812d506e37d33cc031e477affaa2412e
SHA-2084 <2048.sha@gmail.com>
2022-06-07 12:14:33 +0200

    deploy new veGNO with 7 june as unlock start date

--- scripts/deployment/v2/gnosis/deploy_veGNO.ts
+++ scripts/deployment/v2/gnosis/deploy_veGNO.ts
@@ -3,7 +3,7 @@ import hre from "hardhat";
 
 const GNO_TOKEN = "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb";
 const ADMIN = "0xB95842A5E114f5D65b5B96aee42C025331C9417a";
-const unlockTime = 1647302400; // 15 March 2022
+const unlockTime = 1654552800; // 7 June 2022
 
 const network = hre.hardhatArguments.network;
 if (!network) {
