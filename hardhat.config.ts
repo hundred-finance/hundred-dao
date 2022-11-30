643f84596e5bf682d791fc60a511a441199077f9
SHA-2084 <2048.sha@gmail.com>
2022-11-30 13:26:27 +0100

    update hardhat-etherscan deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -84,11 +84,12 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      mainnet: process.env.ETHERSCAN_API_KEY,
-      ropsten: process.env.ETHERSCAN_API_KEY,
-      rinkeby: process.env.ETHERSCAN_API_KEY,
-      goerli: process.env.ETHERSCAN_API_KEY,
-      kovan: process.env.ETHERSCAN_API_KEY,
+      gnosis: <string>process.env.GNOSIS_SCAN_API_KEY,
+      mainnet: <string>process.env.ETHERSCAN_API_KEY,
+      ropsten: <string>process.env.ETHERSCAN_API_KEY,
+      rinkeby: <string>process.env.ETHERSCAN_API_KEY,
+      goerli: <string>process.env.ETHERSCAN_API_KEY,
+      kovan: <string>process.env.ETHERSCAN_API_KEY,
       // binance smart chain
       bsc: "YOUR_BSCSCAN_API_KEY",
       bscTestnet: "YOUR_BSCSCAN_API_KEY",
@@ -96,34 +97,43 @@ const config: HardhatUserConfig = {
       heco: "YOUR_HECOINFO_API_KEY",
       hecoTestnet: "YOUR_HECOINFO_API_KEY",
       // fantom mainnet
-      opera: process.env.FANTOM_SCAN_API_KEY,
-      ftmTestnet: process.env.FANTOM_SCAN_API_KEY,
+      opera: <string>process.env.FANTOM_SCAN_API_KEY,
+      ftmTestnet: <string>process.env.FANTOM_SCAN_API_KEY,
       // optimism
-      optimisticEthereum: process.env.OPTIMISM_SCAN_API_KEY,
-      optimisticKovan: process.env.OPTIMISM_SCAN_API_KEY,
+      optimisticEthereum: <string>process.env.OPTIMISM_SCAN_API_KEY,
+      optimisticKovan: <string>process.env.OPTIMISM_SCAN_API_KEY,
       // polygon
-      polygon: process.env.POLYGON_SCAN_API_KEY,
-      polygonMumbai: process.env.POLYGON_SCAN_API_KEY,
+      polygon: <string>process.env.POLYGON_SCAN_API_KEY,
+      polygonMumbai: <string>process.env.POLYGON_SCAN_API_KEY,
       // arbitrum
-      arbitrumOne: process.env.ARBITRUM_SCAN_API_KEY,
-      arbitrumTestnet: process.env.ARBITRUM_SCAN_API_KEY,
+      arbitrumOne: <string>process.env.ARBITRUM_SCAN_API_KEY,
+      arbitrumTestnet: <string>process.env.ARBITRUM_SCAN_API_KEY,
       // avalanche
-      avalanche: process.env.AVALANCHE_SCAN_API_KEY,
-      avalancheFujiTestnet: process.env.AVALANCHE_SCAN_API_KEY,
+      avalanche: <string>process.env.AVALANCHE_SCAN_API_KEY,
+      avalancheFujiTestnet: <string>process.env.AVALANCHE_SCAN_API_KEY,
       // moonbeam
       moonbeam: "YOUR_MOONBEAM_MOONSCAN_API_KEY",
-      moonriver: process.env.MOONRIVER_SCAN_API_KEY,
+      moonriver: <string>process.env.MOONRIVER_SCAN_API_KEY,
       moonbaseAlpha: "YOUR_MOONBEAM_MOONSCAN_API_KEY",
       // harmony
       harmony: "YOUR_HARMONY_API_KEY",
       harmonyTest: "YOUR_HARMONY_API_KEY",
       // xdai and sokol don't need an API key, but you still need
       // to specify one; any string placeholder will work
-      xdai: "api-key",
       sokol: "api-key",
       aurora: "api-key",
       auroraTestnet: "api-key",
-    }
+    },
+    customChains: [
+      {
+        network: "gnosis",
+        chainId: 100,
+        urls: {
+          apiURL: "https://api.gnosisscan.io/api",
+          browserURL: "https://gnosisscan.io"
+        }
+      }
+    ]
   },
   gasReporter: {
     currency: 'EUR',
